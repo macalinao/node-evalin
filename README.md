69cafb5a67aa1a81f32326ade7765e38a2340599
Ian Macalinao <me@ian.pw>
2015-01-17 17:53:18 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+evalin
+===============
+
+Eval.in API reverse engineered for Node.js
+
+## Installing
+
+```bash
+$ npm install --save evalin
+```
+
+## License
+
+MIT
---
+++ test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var func = require('./');
+
+describe('evalin', function() {
+  it('should ...', function() {
+    expect(true).to.be.true;
+  });
+});

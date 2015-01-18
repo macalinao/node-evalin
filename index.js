7b8ee596f69fc09d6cb7596085b880d013a0852a
Ian Macalinao <me@ian.pw>
2015-01-17 19:24:45 -0600

    Export better language names

--- index.js
+++ index.js
@@ -23,33 +23,87 @@ module.exports = function(code, lang) {
   });
 };
 
-var langs = [
-  'c/gcc-4.4.3',
-  'c/gcc-4.9.1',
-  'c++/c++11-gcc-4.9.1',
-  'c++/gcc-4.4.3',
-  'c++/gcc-4.9.1',
-  'coffeescript/node-0.10.29-coffee-1.7.1',
-  'fortran/f95-4.4.3',
-  'haskell/hugs98-sep-2006',
-  'io/io-20131204',
-  'javascript/node-0.10.29',
-  'lua/lua-5.1.5',
-  'lua/lua-5.2.3',
-  'ocaml/ocaml-4.01.0',
-  'php/php-5.5.14',
-  'pascal/fpc-2.6.4',
-  'perl/perl-5.20.0',
-  'python/cpython-2.7.8',
-  'python/cpython-3.4.1',
-  'ruby/mri-1.0',
-  'ruby/mri-1.8.7',
-  'ruby/mri-1.9.3',
-  'ruby/mri-2.0.0',
-  'ruby/mri-2.1',
-  'ruby/mri-2.2',
-  'slash/slash-head',
-  'assembly/nasm-2.07'
-];
+var langs = [{
+  "id": "Select language",
+  "name": "Select language"
+}, {
+  "id": "c/gcc-4.4.3",
+  "name": "C / GCC 4.4.3"
+}, {
+  "id": "c/gcc-4.9.1",
+  "name": "C / GCC 4.9.1"
+}, {
+  "id": "c++/c++11-gcc-4.9.1",
+  "name": "C++ / C++11 (GCC 4.9.1)"
+}, {
+  "id": "c++/gcc-4.4.3",
+  "name": "C++ / GCC 4.4.3"
+}, {
+  "id": "c++/gcc-4.9.1",
+  "name": "C++ / GCC 4.9.1"
+}, {
+  "id": "coffeescript/node-0.10.29-coffee-1.7.1",
+  "name": "CoffeeScript / CoffeeScript 1.7.1 (Node 0.10.29)"
+}, {
+  "id": "fortran/f95-4.4.3",
+  "name": "Fortran / F95 (GCC 4.4.3)"
+}, {
+  "id": "haskell/hugs98-sep-2006",
+  "name": "Haskell / Hugs98 September 2006"
+}, {
+  "id": "io/io-20131204",
+  "name": "Io / Io 20131204"
+}, {
+  "id": "javascript/node-0.10.29",
+  "name": "JavaScript / Node 0.10.29"
+}, {
+  "id": "lua/lua-5.1.5",
+  "name": "Lua / Lua 5.1.5"
+}, {
+  "id": "lua/lua-5.2.3",
+  "name": "Lua / Lua 5.2.3"
+}, {
+  "id": "ocaml/ocaml-4.01.0",
+  "name": "OCaml / OCaml 4.01.0"
+}, {
+  "id": "php/php-5.5.14",
+  "name": "PHP / PHP 5.5.14"
+}, {
+  "id": "pascal/fpc-2.6.4",
+  "name": "Pascal / Free Pascal 2.6.4"
+}, {
+  "id": "perl/perl-5.20.0",
+  "name": "Perl / Perl 5.20.0"
+}, {
+  "id": "python/cpython-2.7.8",
+  "name": "Python / CPython 2.7.8"
+}, {
+  "id": "python/cpython-3.4.1",
+  "name": "Python / CPython 3.4.1"
+}, {
+  "id": "ruby/mri-1.0",
+  "name": "Ruby / MRI 1.0"
+}, {
+  "id": "ruby/mri-1.8.7",
+  "name": "Ruby / MRI 1.8.7"
+}, {
+  "id": "ruby/mri-1.9.3",
+  "name": "Ruby / MRI 1.9.3"
+}, {
+  "id": "ruby/mri-2.0.0",
+  "name": "Ruby / MRI 2.0.0"
+}, {
+  "id": "ruby/mri-2.1",
+  "name": "Ruby / MRI 2.1"
+}, {
+  "id": "ruby/mri-2.2",
+  "name": "Ruby / MRI 2.2"
+}, {
+  "id": "slash/slash-head",
+  "name": "Slash / Slash HEAD"
+}, {
+  "id": "assembly/nasm-2.07",
+  "name": "x86 Assembly / NASM 2.07"
+}];
 
 module.exports.langs = langs;

d805382eb27db9ccc447bdc2ab670d6135662daa
Ian Macalinao <me@ian.pw>
2015-01-17 18:16:42 -0600

    Fix breaking test

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var evalin = require('./');
 describe('evalin', function() {
   it('should run python', function(done) {
     var lang = 'python/cpython-2.7.8';
-    evalin('print "test"', '').then(function(res) {
+    evalin('print "test"', lang).then(function(res) {
       expect(res.lang).to.equal(lang);
       expect(res.output).to.equal('test\n');
       done();
